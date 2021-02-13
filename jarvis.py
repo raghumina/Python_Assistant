2e4373179558eb810cfcfa9a77acea6dfa4640e6
raghu <raghumina11@gmail.com>
2021-02-13 23:00:22 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -13,6 +13,15 @@ engine.setProperty('voices', voices[0].id)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning")
+
+
+
+
+
 if __name__ == "__main__":
     speak("Hello How are you & what can i do for you")
 
