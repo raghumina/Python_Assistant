69712b523fc338e6919d34fae237c1ad68ecec52
raghu <raghumina11@gmail.com>
2021-02-13 23:05:32 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -1,13 +1,8 @@
 # Virtual Assistant for desktop
 
 
-
-
-
-import pyttsx3    # Library for speak and listen features
-import datetime   # Library for date and time related function
-
-
+import pyttsx3  # Library for speak and listen features
+import datetime  # Library for date and time related function
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -19,12 +14,14 @@ engine.setProperty('voices', voices[0].id)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+
+
 def wishMe():
     hour = int(datetime.datetime.now().hour)
-    if hour>=0 and hour<12:
+    if hour >= 0 and hour < 12:
         speak("Good Morning")
 
-    elif hour>=12 and hour<18:
+    elif hour >= 12 and hour < 18:
         speak("Good Afternoon")
 
     else:
@@ -33,12 +30,5 @@ def wishMe():
     speak("I am your virtual assistant, How may I help you")
 
 
-
-
-
 if __name__ == "__main__":
     speak("Hello How are you & what can i do for you")
-
-
-
-
